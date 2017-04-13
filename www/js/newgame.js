ad8bd0a52614378b9929c9800185cb96fb2dc186
Konrad Luzarowski <konrad.luzarowski@gmail.com>
2017-04-13 20:23:07 +0200

    New game - color balls

--- www/js/newgame.js
+++ www/js/newgame.js
@@ -41,7 +41,72 @@ function screenSize() {
         element.style.width = window.innerWidth * 0.2 + 'px';
         element.style.height = window.innerHeight * 0.1 + 'px';
     }
-    
+    element = document.getElementById('timer');
+    element.style.width = window.innerWidth * 0.5 + 'px';
+    element.style.height = window.innerHeight * 0.1 + 'px';
+    element = document.getElementById('checkbutton');
+    element.style.width = window.innerWidth * 0.5 + 'px';
+    element.style.height = window.innerHeight * 0.1 + 'px';
+    element = document.getElementById('colorbasket');
+    element.style.width = window.innerWidth + 'px';
+    element.style.height = window.innerHeight * 0.1 + 'px';
+    if (sessionStorage.getItem('numbercolors') === '4') {
+        element.innerHTML = '<div class="singlecolorbox"><img class="ball" src="../www/img/white.png"/></div><div class="singlecolorbox"><img class="ball" src="../www/img/blue.png"/></div><div class="singlecolorbox"><img class="ball" src="../www/img/red.png"/></div><div class="singlecolorbox"><img class="ball" src="../www/img/green.png"/></div>'
+        el = document.getElementsByClassName("singlecolorbox");
+        for (i = 0; i < el.length; i++) {
+            element = el.item(i);
+            element.style.width = window.innerWidth * 0.25 + 'px';
+            element.style.height = window.innerHeight * 0.1 + 'px';
+        }
+        el = document.getElementsByClassName("ball");
+        for (i = 0; i < el.length; i++) {
+            element = el.item(i);
+            element.style.width = window.innerHeight * 0.08 + 'px';
+            element.style.height = window.innerHeight * 0.08 + 'px';
+            element.style.marginTop = window.innerHeight * 0.01 + 'px';
+            element.style.marginBottom = window.innerHeight * 0.01 + 'px';
+        }
+    }
+    if (sessionStorage.getItem('numbercolors') === '5') {
+        element.innerHTML = '<div class="singlecolorbox"><img class="ball" src="../www/img/white.png"/></div><div class="singlecolorbox"><img class="ball" src="../www/img/blue.png"/></div><div class="singlecolorbox"><img class="ball" src="../www/img/red.png"/></div><div class="singlecolorbox"><img class="ball" src="../www/img/green.png"/></div><div class="singlecolorbox"><img class="ball" src="../www/img/pink.png"/></div>'
+        el = document.getElementsByClassName("singlecolorbox");
+        for (i = 0; i < el.length; i++) {
+            element = el.item(i);
+            element.style.width = window.innerWidth * 0.2 + 'px';
+            element.style.height = window.innerHeight * 0.1 + 'px';
+        }
+        el = document.getElementsByClassName("ball");
+        for (i = 0; i < el.length; i++) {
+            element = el.item(i);
+            element.style.width = window.innerHeight * 0.08 + 'px';
+            element.style.height = window.innerHeight * 0.08 + 'px';
+            element.style.marginTop = window.innerHeight * 0.01 + 'px';
+            element.style.marginBottom = window.innerHeight * 0.01 + 'px';
+        }
+    }
+    if (sessionStorage.getItem('numbercolors') === '6') {
+        element.innerHTML = '<div class="border"></div><div class="singlecolorbox"><img class="ball" src="../www/img/white.png"/></div><div class="singlecolorbox"><img class="ball" src="../www/img/blue.png"/></div><div class="singlecolorbox"><img class="ball" src="../www/img/red.png"/></div><div class="singlecolorbox"><img class="ball" src="../www/img/green.png"/></div><div class="singlecolorbox"><img class="ball" src="../www/img/pink.png"/></div><div class="singlecolorbox"><img class="ball" src="../www/img/yellow.png"/></div><div class="border"></div>'
+        el = document.getElementsByClassName("singlecolorbox");
+        for (i = 0; i < el.length; i++) {
+            element = el.item(i);
+            element.style.width = window.innerWidth * 0.16 + 'px';
+            element.style.height = window.innerHeight * 0.1 + 'px';
+        }
+        el = document.getElementsByClassName("border");
+        for (i = 0; i < el.length; i++) {
+            element = el.item(i);
+            element.style.width = window.innerWidth * 0.02 + 'px';
+            element.style.height = window.innerHeight * 0.1 + 'px';
+        }
+        el = document.getElementsByClassName("ball");
+        for (i = 0; i < el.length; i++) {
+            element = el.item(i);
+            element.style.width = window.innerHeight * 0.08 + 'px';
+            element.style.height = window.innerHeight * 0.08 + 'px';
+            element.style.marginTop = window.innerHeight * 0.01 + 'px';
+            element.style.marginBottom = window.innerHeight * 0.01 + 'px';
+        }
+    }
 } 
 
 function makeRandomColors() {
@@ -57,10 +122,38 @@ function makeRandomColors() {
         p = p-1;
     }
     return(randomColorsBox);
-    }
+}
 
+function getTime() {
+    var time, toParse;
+    toParse = sessionStorage.getItem('time');
+    switch (toParse) {
+        case '40':
+            time = 40;
+            break;
+        case '30':
+            time = 30;
+            break;
+        case '20':
+            time = 20;
+            break;
+    }
+    return time;
+}
+
+function time() {
+    var elClock = document.getElementById('clock');
+    if (sec>9)
+        elClock.textContent = '00 : ' + sec;
+    else
+        elClock.textContent = '00 : 0' + sec;
+    sec = sec - 1;
+    setTimeout("time();", 1000);
+}
 
 function initGame() {
     makeRandomColors();
     screenSize();
+    sec = getTime();
+    time();
 }
