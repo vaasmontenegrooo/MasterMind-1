ef173aa8f4761a133fd7fa3446bdcfd4e202a616
vaasmontenegrooo <matqus1950@gmail.com>
2017-04-17 12:04:59 +0200

    Drag&Drop06

--- www/js/newgame.js
+++ www/js/newgame.js
@@ -3,29 +3,7 @@ window.addEventListener("load", function () {
     buttonNewGame.addEventListener("click", initGame);
 });
 
-function touchHandler(event) {
-    var touch = event.changedTouches[0];
 
-    var simulatedEvent = document.createEvent("MouseEvent");
-        simulatedEvent.initMouseEvent({
-        touchstart: "mousedown",
-        touchmove: "mousemove",
-        touchend: "mouseup"
-    }[event.type], true, true, window, 1,
-        touch.screenX, touch.screenY,
-        touch.clientX, touch.clientY, false,
-        false, false, false, 0, null);
-
-    touch.target.dispatchEvent(simulatedEvent);
-    event.preventDefault();
-}
-
-function init() {
-    document.addEventListener("touchstart", touchHandler, true);
-    document.addEventListener("touchmove", touchHandler, true);
-    document.addEventListener("touchend", touchHandler, true);
-    document.addEventListener("touchcancel", touchHandler, true);
-}
 
 function randomColor(min, max) {
     var min = parseInt(min, 10);
@@ -219,7 +197,6 @@ function catchSingleBox() {
 
 
 function initGame() {
-    init();
     makeRandomColors();
     screenSize();
     sec = getTime();
